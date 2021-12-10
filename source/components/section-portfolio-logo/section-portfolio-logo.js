0fd525534362e79f563e254efc6db80edf207f75
Igor Melnikov <igor2354@yandex.ru>
2021-12-10 17:14:08 +0300

    edits form chrckbox

--- source/components/section-portfolio-logo/section-portfolio-logo.js
+++ source/components/section-portfolio-logo/section-portfolio-logo.js
@@ -1,14 +1,20 @@
 import Swiper from "swiper/bundle";
 import lightgallery from "lightgallery";
 
-let prototypeSlider = new Swiper(".logo-slider__container", {
-	direction: "vertical",
-	slidesPerView: 2,
-	spaceBetween: 30,
-	loop: true,
-	navigation: {
-		nextEl: ".logo-slider__next",
-	},
-});
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		let prototypeSlider = new Swiper(".logo-slider__container", {
+			direction: "vertical",
+			slidesPerView: 2,
+			spaceBetween: 30,
+			loop: true,
+			navigation: {
+				nextEl: ".logo-slider__next",
+			},
+		});
 
-lightgallery(document.querySelector(".logo-slider.lightgallery"), { selector: "a" });
+		lightgallery(document.querySelector(".logo-slider.lightgallery"), { selector: "a" });
+	},
+	false
+);
--- source/components/section-portfolio-mobile/section-portfolio-mobile.js
+++ source/components/section-portfolio-mobile/section-portfolio-mobile.js
@@ -1,49 +1,55 @@
 import Swiper from "swiper/bundle";
 
-let prototypeSlider = new Swiper(".mobile-slider__container", {
-	slidesPerView: "auto",
-	loop: true,
-	watchSlidesProgress: 1,
-	centeredSlides: true,
-	navigation: {
-		nextEl: ".mobile-slider__next",
-	},
-	breakpoints: {
-		1170: {
-			centeredSlides: false,
-		},
-	},
-	on: {
-		progress(swiper) {
-			const t = swiper.slides.length;
-			for (let r = 0; r < swiper.slides.length; r += 1) {
-				const slideEl = swiper.slides[r],
-					slideElProgress = swiper.slides[r].progress,
-					slideElProgressAbs = Math.abs(slideElProgress);
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		let prototypeSlider = new Swiper(".mobile-slider__container", {
+			slidesPerView: "auto",
+			loop: true,
+			watchSlidesProgress: 1,
+			centeredSlides: true,
+			navigation: {
+				nextEl: ".mobile-slider__next",
+			},
+			breakpoints: {
+				1170: {
+					centeredSlides: false,
+				},
+			},
+			on: {
+				progress(swiper) {
+					const t = swiper.slides.length;
+					for (let r = 0; r < swiper.slides.length; r += 1) {
+						const slideEl = swiper.slides[r],
+							slideElProgress = swiper.slides[r].progress,
+							slideElProgressAbs = Math.abs(slideElProgress);
 
-				let counter = 1;
-				slideElProgressAbs > 1 && (counter = 0.3 * (slideElProgressAbs - 1) + 1);
-				const l = slideEl.querySelectorAll(".carousel-slider-animate-opacity"),
-					c = 1 - 0.1 * slideElProgressAbs,
-					a = slideElProgress * counter * (100 * ((1 - c) / 2.5)),
-					d = t - Math.abs(Math.round(slideElProgress));
-				(slideEl.style.transform = `scale(${c.toFixed(1)}) translateX(${a.toFixed(1)}%) `),
-					(slideEl.style.zIndex = d),
-					(slideEl.style.opacity = slideElProgressAbs > 3 ? 0 : 1),
-					l.forEach((swiper) => {
-						swiper.style.opacity = 1 - slideElProgressAbs / 3;
-					});
-			}
-		},
-		setTransition(swiper, t) {
-			for (let r = 0; r < swiper.slides.length; r += 1) {
-				const slideEl = swiper.slides[r],
-					slideElProgress = slideEl.querySelectorAll(".carousel-slider-animate-opacity");
-				(slideEl.style.transitionDuration = `${t}ms`),
-					slideElProgress.forEach((swiper) => {
-						swiper.style.transitionDuration = `${t}ms`;
-					});
-			}
-		},
+						let counter = 1;
+						slideElProgressAbs > 1 && (counter = 0.3 * (slideElProgressAbs - 1) + 1);
+						const l = slideEl.querySelectorAll(".carousel-slider-animate-opacity"),
+							c = 1 - 0.1 * slideElProgressAbs,
+							a = slideElProgress * counter * (100 * ((1 - c) / 2.5)),
+							d = t - Math.abs(Math.round(slideElProgress));
+						(slideEl.style.transform = `scale(${c.toFixed(1)}) translateX(${a.toFixed(1)}%) `),
+							(slideEl.style.zIndex = d),
+							(slideEl.style.opacity = slideElProgressAbs > 3 ? 0 : 1),
+							l.forEach((swiper) => {
+								swiper.style.opacity = 1 - slideElProgressAbs / 3;
+							});
+					}
+				},
+				setTransition(swiper, t) {
+					for (let r = 0; r < swiper.slides.length; r += 1) {
+						const slideEl = swiper.slides[r],
+							slideElProgress = slideEl.querySelectorAll(".carousel-slider-animate-opacity");
+						(slideEl.style.transitionDuration = `${t}ms`),
+							slideElProgress.forEach((swiper) => {
+								swiper.style.transitionDuration = `${t}ms`;
+							});
+					}
+				},
+			},
+		});
 	},
-});
+	false
+);
