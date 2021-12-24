663840af001b9cccc851b8648eaeeb731ffcc4bd
Igor Melnikov <igor2354@yandex.ru>
2021-12-24 17:58:58 +0300

    edits discrtiption

--- source/js/seo-advert.js
+++ source/js/seo-advert.js
@@ -1,5 +1,4 @@
 import sectionSeoAdvert from "../components/section-seo-advert/section-seo-advert";
-import sectionEexamplesWorks from "../components/section-examples-works/section-examples-works";
 import sectionSliderTariff from "../components/section-slider-tariff/section-slider-tariff";
 
 import { loadAnimation } from "lottie-web";
